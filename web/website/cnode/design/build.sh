ca96ac3f70c90f791075b76e722ca70db35e9b6c
zhbhun <zhbhun@gmail.com>
2017-01-19 10:26:10 +0800

    add website cnode

--- web/website/cnode/design/build.sh
+++
@@ -1,4 +0,0 @@
-lessc ./less/common/index.less ./css/common.css --source-map
-lessc ./less/base/index.less ./css/base.css --source-map
-lessc ./less/home/index.less ./css/home.css --source-map
-lessc ./less/detail/index.less ./css/detail.css --source-map
